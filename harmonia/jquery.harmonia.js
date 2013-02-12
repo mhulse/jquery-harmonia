f5d865326f003df65043dcbf442511c301210ea4
Micky Hulse <micky@registerguard.com>
2013-02-11 16:18:32 -0800

    Mmmmm, changes are gooooood. :)

--- harmonia/jquery.harmonia.js
+++ harmonia/jquery.harmonia.js
@@ -24,7 +24,7 @@
 
 ;(function($, window, document, undefined) {
 	
-	'use strict';
+	'use strict'; // Lint setting.
 	
 	//--------------------------------------------------------------------------
 	//
@@ -32,9 +32,9 @@
 	//
 	//--------------------------------------------------------------------------
 	
-	var console = window.console || { log : function() {}, warn : function() {} },
+	var console = window.console || { log : function() {}, warn : function() {} },  // Javascript console.
 	
-	NS = 'harmonia',
+	NS = 'harmonia', // The plugin namespace.
 	
 	//--------------------------------------------------------------------------
 	//
@@ -51,7 +51,7 @@
 		init : function(opts) {
 			
 			//----------------------------------
-			// Loop & return each `this`:
+			// Loop & return each this:
 			//----------------------------------
 			
 			return this.each(function() {
@@ -62,7 +62,7 @@
 				
 				var $this   = $(this),                                                  // Target object.
 				    data    = $this.data(NS),                                           // Namespace instance data.
-				    options = $.extend({}, settings.external, $.fn[NS].defaults, opts); // Merge `settings`, `defaults` and `opts`.
+				    options = $.extend({}, settings.external, $.fn[NS].defaults, opts); // Merge settings, defaults and opts.
 				
 				//----------------------------------
 				// Initialize data:
@@ -78,7 +78,7 @@
 					    $select = $('<select>', { 'class' : options.selectClass });
 					
 					//----------------------------------
-					// Namespaced instance `data`:
+					// Namespaced instance data:
 					//----------------------------------
 					
 					$this.data(NS, {
@@ -87,12 +87,12 @@
 						init    : false,
 						matched : false,
 						options : options,
-						target  : $this,
-						select  : $select
+						select  : $select,
+						target  : $this
 						
 					});
 					
-					data = $this.data(NS); // Make it easy for the rest of `init()`.
+					data = $this.data(NS); // Make it easy for the rest of init.
 					
 				}
 				
@@ -103,179 +103,41 @@
 				if ( ! data.init) {
 					
 					//----------------------------------
-					// Data initialization flag:
+					// Call main:
 					//----------------------------------
 					
-					data.init = true;
-					
-					//----------------------------------
-					// Callback:
-					//----------------------------------
-					
-					data.options.onInit.call(data.target);
-					
-					//----------------------------------
-					// Check for object(s):
-					//----------------------------------
-					
-					if (data.hrefs.length) {
-						
-						//----------------------------------
-						// Root menu CSS class:
-						//----------------------------------
-						
-						data.target.addClass(settings.internal.initClass);
-						
-						//----------------------------------
-						// Default `<select>` `<option>`?
-						//----------------------------------
-						
-						if (data.options.defaultOption) {
-							
-							//----------------------------------
-							// Get the `<option>`:
-							//----------------------------------
-							
-							var $default = optionize.call(data.target, $('<a />'), data.options.defaultOption);
-							
-							//----------------------------------
-							// Append `<option>` to `<select>`:
-							//----------------------------------
-							
-							if ($default.length) {
-								
-								$default.appendTo(data.select);
-								
-							}
-							
-						}
-						
-						//----------------------------------
-						// Create the other `<option>`s:
-						//----------------------------------
-						
-						data.hrefs.each(function() {
-							
-							//----------------------------------
-							// Get the `<option>`:
-							//----------------------------------
-							
-							var $option = optionize.call(data.target, $(this));
-							
-							//----------------------------------
-							// Append `<option>` to `<select>`:
-							//----------------------------------
-							
-							if ($option.length) {
-								
-								$option.appendTo(data.select);
-								
-							}
-							
-						});
-						
-						//----------------------------------
-						// Add change event to `<select>`:
-						//----------------------------------
-						
-						data.select.change(function() {
-							
-							//----------------------------------
-							// Local variable(s):
-							//----------------------------------
-							
-							var $$ = $(this);
-							
-							//----------------------------------
-							// Callback:
-							//----------------------------------
-							
-							data.options.onChange.call(data.target, $$); // @TODO: Is this the best spot for this?
-							
-							//----------------------------------
-							// Get link value:
-							//----------------------------------
-							
-							var val = $$.val();
-							
-							//----------------------------------
-							// Follow link value?
-							//----------------------------------
-							
-							if (val && (val !== '#')) { // @TODO: Improve link validation?
-								
-								//----------------------------------
-								// Ignore default `<select>`:
-								//----------------------------------
-								
-								if (val !== data.options.defaultOption) {
-									
-									//----------------------------------
-									// Open tab or use current window:
-									//----------------------------------
-									
-									if (data.options.openTab) {
-										
-										window.open(val); // New tab.
-										
-									} else {
-										
-										window.location = val; // Current window.
-										
-									}
-									
-								}
-								
-							}
-							
-						})
-						
-						//----------------------------------
-						// Insert before target:
-						//----------------------------------
-						
-						.insertBefore(data.target);
-						
-						//----------------------------------
-						// Callback:
-						//----------------------------------
-						
-						data.options.onAfterInit.call(data.target);
-						
-					} else {
-						
-						console.warn('there was a problem with your markup');
-						
-						return this;
-						
-					}
+					_main.call($this, data);
 					
 				} else {
 					
-					console.warn(NS, 'already initialized on', this);
+					//----------------------------------
+					// Ouch!
+					//----------------------------------
 					
-					return this;
+					console.warn('jQuery.' + NS, 'thinks it\'s already initialized on', this);
+					
+					//return this; // Needed?
 					
 				}
 				
 			});
 			
-		}, // init()
+		}, // init
 		
-		//--------------------------------------------------------------------
+		//----------------------------------
 		
 		/**
 		 * Removes plugin from element.
 		 *
-		 * @type   { function }
-		 * @this   { object.jquery }
+		 * @type { function }
+		 * @this { object.jquery }
 		 * @return { object.jquery } Returns target object(s) for chaining purposes.
 		 */
 		
 		destroy : function() {
 			
 			//----------------------------------
-			// Loop & return each `this`:
+			// Loop & return each this:
 			//----------------------------------
 			
 			return this.each(function() {
@@ -284,8 +146,8 @@
 				// Local variable(s):
 				//----------------------------------
 				
-				var $$   = $(this),
-				    data = $$.data(NS);
+				var $this = $(this),
+				    data  = $this.data(NS);
 				
 				//----------------------------------
 				// Data?
@@ -303,7 +165,7 @@
 					// Remove root menu CSS class:
 					//----------------------------------
 					
-					$$.removeClass(settings.internal.initClass);
+					$this.removeClass(settings.internal.initClass);
 					
 					//----------------------------------
 					// Remove generated HTML:
@@ -315,13 +177,13 @@
 					// Namespaced instance data:
 					//----------------------------------
 					
-					$$.removeData(NS);
+					$this.removeData(NS);
 					
 				}
 			
 			});
 			
-		} // foo()
+		} // destroy
 		
 	}, // methods
 	
@@ -332,15 +194,203 @@
 	//--------------------------------------------------------------------------
 	
 	/**
+	 * Called after plugin initialization.
+	 *
+	 * @private
+	 * @type { function }
+	 * @this { object.jquery }
+	 */
+	
+	_main = function(data) {
+		
+		//----------------------------------
+		// Data?
+		//----------------------------------
+		
+		if (typeof data == 'undefined') {
+			
+			//----------------------------------
+			// Attempt to determine data:
+			//----------------------------------
+			
+			data = this.data(NS);
+			
+		}
+		
+		//----------------------------------
+		// Data?
+		//----------------------------------
+		
+		if (data) {
+			
+			//----------------------------------
+			// Yup!
+			//----------------------------------
+			
+			data.init = true; // Data initialization flag.
+			
+			//----------------------------------
+			// Callback:
+			//----------------------------------
+			
+			data.options.onInit.call(data.target);
+			
+			//----------------------------------
+			// Check for object(s):
+			//----------------------------------
+			
+			if (data.hrefs.length) {
+				
+				//----------------------------------
+				// Root menu CSS class:
+				//----------------------------------
+				
+				data.target.addClass(settings.internal.initClass);
+				
+				//----------------------------------
+				// Default `<select>` `<option>`?
+				//----------------------------------
+				
+				if (data.options.defaultOption) {
+					
+					//----------------------------------
+					// Get the `<option>`:
+					//----------------------------------
+					
+					var $default = _optionize.call(data.target, $('<a />'), data.options.defaultOption);
+					
+					//----------------------------------
+					// Append `<option>` to `<select>`:
+					//----------------------------------
+					
+					if ($default.length) {
+						
+						$default.appendTo(data.select);
+						
+					}
+					
+				}
+				
+				//----------------------------------
+				// Create the other `<option>`s:
+				//----------------------------------
+				
+				data.hrefs.each(function() {
+					
+					//----------------------------------
+					// Get the `<option>`:
+					//----------------------------------
+					
+					var $option = _optionize.call(data.target, $(this));
+					
+					//----------------------------------
+					// Append `<option>` to `<select>`:
+					//----------------------------------
+					
+					if ($option.length) {
+						
+						$option.appendTo(data.select);
+						
+					}
+					
+				});
+				
+				//----------------------------------
+				// Add change event to `<select>`:
+				//----------------------------------
+				
+				data.select.change(function() {
+					
+					//----------------------------------
+					// Local variable(s):
+					//----------------------------------
+					
+					var $this = $(this);
+					
+					//----------------------------------
+					// Callback:
+					//----------------------------------
+					
+					data.options.onChange.call(data.target, $this); // @TODO: Is this the best spot for this?
+					
+					//----------------------------------
+					// Get link value:
+					//----------------------------------
+					
+					var val = $this.val();
+					
+					//----------------------------------
+					// Follow link value?
+					//----------------------------------
+					
+					if (val && (val !== '#')) { // @TODO: Improve link validation?
+						
+						//----------------------------------
+						// Ignore default `<select>`:
+						//----------------------------------
+						
+						if (val !== data.options.defaultOption) {
+							
+							//----------------------------------
+							// Open tab or use current window:
+							//----------------------------------
+							
+							if (data.options.openTab) {
+								
+								window.open(val); // New tab.
+								
+							} else {
+								
+								window.location = val; // Current window.
+								
+							}
+							
+						}
+						
+					}
+					
+				})
+				
+				//----------------------------------
+				// Insert before target:
+				//----------------------------------
+				
+				.insertBefore(data.target);
+				
+				//----------------------------------
+				// Callback:
+				//----------------------------------
+				
+				data.options.onAfterInit.call(data.target);
+				
+			} else {
+				
+				//----------------------------------
+				// Problemos:
+				//----------------------------------
+				
+				console.warn('jQuery.' + NS, 'thinks there\'s a problem with your markup');
+				
+			}
+			
+			
+		}
+		
+	}, // _main
+	
+	//----------------------------------
+	
+	/**
 	 * Create options for the `<select>` menu.
 	 *
-	 * @this   { object.jquery } The base target object.
-	 * @param  { object.jquery } The `<a>` to convert a `<select>`.
-	 * @param  { string } text Optional text for `<option>`.
+	 * @private
+	 * @this { object.jquery } The base target object.
+	 * @param { object.jquery } The `<a>` to convert a `<select>`.
+	 * @param { string } text Optional text for `<option>`.
 	 * @return { * } The `<select>` `<option>` or an empty string.
 	 */
 	
-	optionize = function($a, text) {
+	_optionize = function($a, text) {
 		
 		//----------------------------------
 		// Local variable(s):
@@ -349,13 +399,21 @@
 		var $return = '',
 		    data    = this.data(NS);
 		
+		//----------------------------------
+		// Data?
+		//----------------------------------
+		
 		if (data) {
 			
 			//----------------------------------
 			// Default arg values:
 			//----------------------------------
 			
-			text = ((typeof text !== 'undefined') && text.length) ? text : textualize.call($a); // Use arg or `<a>` text.
+			text = ((typeof text != 'undefined') && text.length) ? text : _textualize.call($a); // Use arg or `<a>` text.
+			
+			//----------------------------------
+			// Text?
+			//----------------------------------
 			
 			if (text.length) {
 				
@@ -392,7 +450,7 @@
 							var href = $(location).attr('href');
 							
 							//----------------------------------
-							// Compare directly & indexOf():
+							// Compare directly & `indexOf()`:
 							//----------------------------------
 							
 							if ((uri == href) || (href.indexOf(uri) > 0)) {
@@ -412,7 +470,7 @@
 					if (selected) {
 						
 						//----------------------------------
-						// Set `selected` attribute:
+						// Set selected attribute:
 						//----------------------------------
 						
 						$option.attr('selected', 'selected'); // Pheeeew. :D
@@ -421,7 +479,7 @@
 						// Set flag and stop checking:
 						//----------------------------------
 						
-						data.matched = true; // Flippin' switches.
+						data.matched = true; // Flippin' switches!
 						
 					}
 					
@@ -441,7 +499,11 @@
 				
 			} else {
 				
-				console.warn('there\'s no text for', this);
+				//----------------------------------
+				// Oopsies:
+				//----------------------------------
+				
+				console.warn('jQuery.' + NS, 'thinks there\'s no text for', this);
 				
 			}
 			
@@ -459,23 +521,24 @@
 		
 		return $return;
 		
-	}, // optionize()
+	}, // _optionize
 	
-	//--------------------------------------------------------------------
+	//----------------------------------
 	
 	/**
 	 * Get text value of a jQuery object, but not its children.
 	 *
-	 * @see    rgne.ws/Rj0KNX
-	 * @this   { object.jquery } Any jQuery object.
+	 * @private
+	 * @see rgne.ws/Rj0KNX
+	 * @this { object.jquery } Any jQuery object.
 	 * @return { * } The object's text or an empty string.
 	 */
 	
-	textualize = function() {
+	_textualize = function() {
 		
 		return (this.length) ? this.clone().children().remove().end().text() : '';
 		
-	}; // textualize()
+	}; // _textualize
 	
 	//--------------------------------------------------------------------------
 	//
@@ -486,12 +549,11 @@
 	/**
 	 * Boilerplate plugin logic.
 	 *
-	 * @see    rgne.ws/OvKpPc
-	 * @type   { function }
-	 * @param  { string } method String method identifier.
-	 * @return { method } Calls plugin method with supplied params.
-	 *
 	 * @constructor
+	 * @see rgne.ws/OvKpPc
+	 * @type { function }
+	 * @param { string } method String method identifier.
+	 * @return { method } Calls plugin method with supplied params.
 	 */
 	
 	$.fn[NS] = function(method) {
@@ -504,7 +566,7 @@
 			
 			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
 			
-		} else if ((typeof method === 'object') || ( ! method)) {
+		} else if ((typeof method == 'object') || ( ! method)) {
 			
 			return methods.init.apply(this, arguments);
 			
@@ -514,11 +576,11 @@
 			
 		}
 		
-	}; // constructor()
+	}; // $.fn[NS]
 	
 	//--------------------------------------------------------------------------
 	//
-	// Default settings:
+	// Defaults and settings:
 	//
 	//--------------------------------------------------------------------------
 	
@@ -531,9 +593,11 @@
 	var settings = {}; // Initialize config object.
 	
 	//----------------------------------
+	
 	/**
 	 * Private settings.
 	 *
+	 * @private
 	 * @type { object }
 	 */
 	
